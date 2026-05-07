f32c3c4ef57e06acd04cbb15d5ae040984ef4011
Kostiantyn <konstanlebed8@gmail.com>
2026-05-07 12:27:13 +0200

    restructure App component and enhance main entry with routing and state management

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
 
-createRoot(document.getElementById('root')).render(
+import CssBaseline from "@mui/material/CssBaseline";
+import "./index.css";
+
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./theme";
+
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <CssBaseline />
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>,
-)
+);
