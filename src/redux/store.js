dea3abaa4aa2743f275edc7738d0413bc0352417
Kostiantyn <konstanlebed8@gmail.com>
2026-05-12 13:04:54 +0200

    feat: add auth reducer to the Redux store configuration

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postsReducer from "./slices/postsSlice.js";
+import authReducer from "./slices/authSlice.js";
 
 export const store = configureStore({
   reducer: {
     posts: postsReducer,
+    auth: authReducer,
   },
 });
 
