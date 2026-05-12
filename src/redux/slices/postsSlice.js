89b01fa53b35347fdac51aed1fbb521a2437da69
Kostiantyn <konstanlebed8@gmail.com>
2026-05-12 12:50:03 +0200

    feat: implement async thunks for fetching posts and tags, and deleting posts

--- src/redux/slices/postsSlice.js
+++ src/redux/slices/postsSlice.js
@@ -1,8 +1,28 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "../../axios";
+
+export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
+  const { data } = await axios.get("/posts");
+  console.log(data);
+  return data;
+});
+
+export const fetchTags = createAsyncThunk("/posts/fetchTags", async () => {
+  const { data } = await axios.get("/tags");
+  return data;
+});
+
+export const fetchRemovePost = createAsyncThunk(
+  "/posts/fetchRemovePost",
+  async (id) => {
+    const { data } = await axios.delete(`/posts/${id}`);
+    return data;
+  },
+);
 
 const initialState = {
   posts: {
-    items: {},
+    items: [],
     status: "loading",
   },
   tags: {
@@ -15,6 +35,38 @@ export const postSlice = createSlice({
   name: "posts",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    //get posts  --------------------------------------------
+    builder.addCase(fetchPosts.pending, (state) => {
+      state.posts.status = "loading";
+    });
+    builder.addCase(fetchPosts.fulfilled, (state, action) => {
+      state.posts.items = action.payload;
+      state.posts.status = "loaded";
+    });
+    builder.addCase(fetchPosts.rejected, (state) => {
+      state.posts.items = [];
+      state.posts.status = "error";
+    });
+    //get tags  --------------------------------------------
+    builder.addCase(fetchTags.pending, (state) => {
+      state.tags.status = "loading";
+    });
+    builder.addCase(fetchTags.fulfilled, (state, action) => {
+      state.tags.items = action.payload;
+      state.tags.status = "loaded";
+    });
+    builder.addCase(fetchTags.rejected, (state) => {
+      state.tags.items = [];
+      state.tags.status = "error";
+    });
+    //delete post  --------------------------------------------
+    builder.addCase(fetchRemovePost.pending, (state, action) => {
+      state.posts.items = state.posts.items.filter(
+        (obj) => obj._id !== action.meta.arg,
+      );
+    });
+  },
 });
 
 export default postSlice.reducer;
