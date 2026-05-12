9db960cbad7a1a1efe40df018fd47be5479cc5f0
Kostiantyn <konstanlebed8@gmail.com>
2026-05-12 13:12:00 +0200

    feat: fetch posts and tags data on Home component mount, enhance loading state handling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,56 +1,95 @@
-import React from 'react';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Grid from '@mui/material/Grid';
+import React, { useEffect } from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Grid from "@mui/material/Grid";
 
-import { Post } from '../components/Post';
-import { TagsBlock } from '../components/TagsBlock';
-import { CommentsBlock } from '../components/CommentsBlock';
+import axios from "@/axios";
+
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectPostsData,
+  selectPosts,
+  selectPostsTags,
+} from "@/redux/selectors/postsSelectors";
+import { selectTags, selectTagsData } from "@/redux/selectors/tagsSelectors";
+import { fetchPosts, fetchTags } from "@/redux/slices/postsSlice";
+import {
+  selectIsAuth,
+  selectUserData,
+} from "@/redux/selectors/authSelectors.js";
+
+import { Post } from "../components/Post";
+import { TagsBlock } from "../components/TagsBlock";
+import { CommentsBlock } from "../components/CommentsBlock";
 
 export const Home = () => {
+  const dispatch = useDispatch();
+  const userData = useSelector(selectUserData);
+  console.log(userData);
+
+  const postsData = useSelector(selectPostsData);
+  const tagsData = useSelector(selectTagsData);
+  const posts = useSelector(selectPosts);
+  console.log(postsData);
+  console.log(tagsData);
+
+  const isPostsLoading = postsData.status === "loading";
+  const isTagsLoading = tagsData.status === "loading";
+
+  useEffect(() => {
+    dispatch(fetchPosts());
+    dispatch(fetchTags());
+  }, []);
+
   return (
     <>
-      <Tabs style={{ marginBottom: 15 }} value={0} aria-label="basic tabs example">
+      <Tabs
+        style={{ marginBottom: 15 }}
+        value={0}
+        aria-label="basic tabs example"
+      >
         <Tab label="Новые" />
         <Tab label="Популярные" />
       </Tabs>
       <Grid container spacing={4}>
-        <Grid xs={8} item>
-          {[...Array(5)].map(() => (
+        <Grid size={8}>
+          {(isPostsLoading ? [...Array(5)] : posts).map((postElem, index) => (
             <Post
-              id={1}
-              title="Roast the code #1 | Rock Paper Scissors"
-              imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
-              user={{
-                avatarUrl:
-                  'https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png',
-                fullName: 'Keff',
-              }}
-              createdAt={'12 июня 2022 г.'}
-              viewsCount={150}
+              key={postElem?._id || index}
+              isLoading={isPostsLoading}
+              id={postElem?._id}
+              title={postElem?.title}
+              imageUrl={
+                postElem?.imageUrl
+                  ? `http://localhost:4444${postElem.imageUrl}`
+                  : ""
+              }
+              user={postElem?.user}
+              createdAt={postElem?.createdAt}
+              viewsCount={postElem?.viewsCount}
               commentsCount={3}
-              tags={['react', 'fun', 'typescript']}
-              isEditable
+              tags={postElem?.tags}
+              isEditable={userData?._id === postElem?.user._id}
             />
           ))}
         </Grid>
-        <Grid xs={4} item>
-          <TagsBlock items={['react', 'typescript', 'заметки']} isLoading={false} />
+        <Grid size={4}>
+          <TagsBlock items={tagsData.items} isLoading={isTagsLoading} />
           <CommentsBlock
             items={[
               {
                 user: {
-                  fullName: 'Вася Пупкин',
-                  avatarUrl: 'https://mui.com/static/images/avatar/1.jpg',
+                  fullName: "Вася Пупкин",
+                  avatarUrl: "https://mui.com/static/images/avatar/1.jpg",
                 },
-                text: 'Это тестовый комментарий',
+                text: "Это тестовый комментарий",
               },
               {
                 user: {
-                  fullName: 'Иван Иванов',
-                  avatarUrl: 'https://mui.com/static/images/avatar/2.jpg',
+                  fullName: "Иван Иванов",
+                  avatarUrl: "https://mui.com/static/images/avatar/2.jpg",
                 },
-                text: 'When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top',
+                text: "When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top",
               },
             ]}
             isLoading={false}
