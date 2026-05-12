d5293cb70ab586b4aadae992d29b5743a7290487
Kostiantyn <konstanlebed8@gmail.com>
2026-05-12 13:10:36 +0200

    feat: implement login form with authentication and error handling

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,28 +1,82 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 
+import { useForm } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { fetchAuth } from "@/redux/slices/authSlice.js";
+import { selectIsAuth } from "@/redux/selectors/authSelectors.js";
+
 import styles from "./Login.module.scss";
 
 export const Login = () => {
+  const dispatch = useDispatch();
+  const isAuth = useSelector(selectIsAuth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuth) {
+      navigate("/");
+      console.log("Navigate resended on main page");
+    }
+  }, [isAuth, navigate]);
+
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isValid },
+  } = useForm({
+    defaultValues: {
+      email: "nytipkonechno2@test.com",
+      password: "StrongPass12222",
+    },
+    mode: "onChange",
+  });
+
+  const onSubmit = async (values) => {
+    const data = await dispatch(fetchAuth(values));
+
+    if (!data.payload) {
+      return alert("Authorization is failed");
+    }
+
+    if ("token" in data.payload) {
+      window.localStorage.setItem("token", data.payload.token);
+    }
+  };
+
   return (
-    <Paper classes={{ root: styles.root }}>
+    <Paper elevation={0} classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
-        Вход в аккаунт
+        Sign in account
       </Typography>
-      <TextField
-        className={styles.field}
-        label="E-Mail"
-        error
-        helperText="Неверно указана почта"
-        fullWidth
-      />
-      <TextField className={styles.field} label="Пароль" fullWidth />
-      <Button size="large" variant="contained" fullWidth>
-        Войти
-      </Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          className={styles.field}
+          label="E-Mail"
+          error={Boolean(errors.email?.message)}
+          helperText={errors.email?.message}
+          type="email"
+          {...register("email", { required: "The email is required" })}
+          fullWidth
+        />
+        <TextField
+          className={styles.field}
+          label="Password"
+          type="password"
+          error={Boolean(errors.password?.message)}
+          helperText={errors.password?.message}
+          fullWidth
+          {...register("password", { required: "The password is required" })}
+        />
+        <Button type="submit" size="large" variant="contained" fullWidth>
+          Log in
+        </Button>
+      </form>
     </Paper>
   );
 };
